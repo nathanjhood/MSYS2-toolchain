966955417ea4fc48ae93587342d00763b4cd5d39
StoneyDSP <nathanjhood@googlemail.com>
2023-07-05 14:53:56 +0200

    Enhanced 'MSYS2' toolchain file

--- scripts/toolchains/MINGW64.cmake
+++ scripts/toolchains/MINGW64.cmake
@@ -1,5 +1,6 @@
 if(NOT _MSYS_MINGW64_TOOLCHAIN)
     set(_MSYS_MINGW64_TOOLCHAIN 1)
+
     message(STATUS "MinGW x64 toolchain loading...")
 
     # set(CMAKE_MODULE_PATH)
@@ -77,13 +78,15 @@ if(NOT _MSYS_MINGW64_TOOLCHAIN)
 
     if(MSYS_TARGET_ARCHITECTURE STREQUAL "x86")
         set(CMAKE_SYSTEM_PROCESSOR i686 CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable.")
-     elseif(MSYS_TARGET_ARCHITECTURE STREQUAL "x64")
+    elseif(MSYS_TARGET_ARCHITECTURE STREQUAL "x64")
         set(CMAKE_SYSTEM_PROCESSOR x86_64 CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable.")
-     elseif(MSYS_TARGET_ARCHITECTURE STREQUAL "arm")
+    elseif(MSYS_TARGET_ARCHITECTURE STREQUAL "arm")
         set(CMAKE_SYSTEM_PROCESSOR armv7 CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable.")
-     elseif(MSYS_TARGET_ARCHITECTURE STREQUAL "arm64")
+    elseif(MSYS_TARGET_ARCHITECTURE STREQUAL "arm64")
         set(CMAKE_SYSTEM_PROCESSOR aarch64 CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable.")
-     endif()
+    endif()
+    #set(CMAKE_SYSTEM_PROCESSOR "x86_64" CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable." FORCE) # include(Platform/${CMAKE_EFFECTIVE_SYSTEM_NAME}-${CMAKE_CXX_COMPILER_ID}-CXX-${CMAKE_SYSTEM_PROCESSOR} OPTIONAL RESULT_VARIABLE _INCLUDED_FILE)                             #CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable." FORCE)
+
 
     # Targets for vars
 
@@ -91,12 +94,11 @@ if(NOT _MSYS_MINGW64_TOOLCHAIN)
     # Need to override MinGW from MSYS_CMAKE_SYSTEM_NAME
     set(CMAKE_SYSTEM_NAME "MINGW64" CACHE STRING "The name of the operating system for which CMake is to build." FORCE)
 
-    #set(CMAKE_SYSTEM_PROCESSOR "x86_64" CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable." FORCE) # include(Platform/${CMAKE_EFFECTIVE_SYSTEM_NAME}-${CMAKE_CXX_COMPILER_ID}-CXX-${CMAKE_SYSTEM_PROCESSOR} OPTIONAL RESULT_VARIABLE _INCLUDED_FILE)                             #CACHE STRING "When not cross-compiling, this variable has the same value as the ``CMAKE_HOST_SYSTEM_PROCESSOR`` variable." FORCE)
 
 
     foreach(lang C CXX ASM Fortran OBJC OBJCXX)
         ##-- CMakeCXXInformation: include(Compiler/<CMAKE_CXX_COMPILER_ID>-<LANG>)
-        ##-- set(CMAKE_${lang}_COMPILER_ID "GNU" CACHE STRING "" FORCE) - actually, let's fallback to Kitware's GNU
+        #set(CMAKE_${lang}_COMPILER_ID "MINGW64 13.1.0" CACHE STRING "" FORCE) # - actually, let's fallback to Kitware's GNU
         ##-- 'TARGET' tells the compiler in question what it's '--target:' is.
         set(CMAKE_${lang}_COMPILER_TARGET "x86_64-w64-mingw32" CACHE STRING "The target for cross-compiling, if supported. '--target=x86_64-w64-mingw32'")
 
@@ -108,6 +110,18 @@ if(NOT _MSYS_MINGW64_TOOLCHAIN)
 
     find_program(CMAKE_CXX_COMPILER "${Z_MINGW64_ROOT_DIR}/bin/x86_64-w64-mingw32-g++.exe")
     mark_as_advanced(CMAKE_CXX_COMPILER)
+
+    find_program(CMAKE_RC_COMPILER "${Z_MINGW64_ROOT_DIR}/bin/windres.exe")
+    mark_as_advanced(CMAKE_RC_COMPILER)
+
+    find_program(CMAKE_ASM_COMPILER "${Z_MINGW64_ROOT_DIR}/bin/as.exe")
+    mark_as_advanced(CMAKE_ASM_COMPILER)
+
+    find_program(CMAKE_OBJCXX_COMPILER "${Z_MINGW64_ROOT_DIR}/bin/x86_64-w64-mingw32-gcc.exe")
+    mark_as_advanced(CMAKE_OBJC_COMPILER)
+
+    find_program(CMAKE_OBJCXX_COMPILER "${Z_MINGW64_ROOT_DIR}/bin/x86_64-w64-mingw32-g++.exe")
+    mark_as_advanced(CMAKE_OBJCXX_COMPILER)
 
     find_program(CMAKE_RC_COMPILER "${Z_MINGW64_ROOT_DIR}/bin/windres.exe")
     mark_as_advanced(CMAKE_RC_COMPILER)
@@ -125,8 +139,28 @@ if(NOT _MSYS_MINGW64_TOOLCHAIN)
     # The following flags come from 'PORT' files (i.e., build config files for packages)
     if(NOT _CMAKE_IN_TRY_COMPILE)
 
-        set(LDFLAGS "-pipe")
+        set(LDFLAGS)
+        string(APPEND LDFLAGS " -pipe")
+        set(LDFLAGS "${LDFLAGS}")
         set(ENV{LDFLAGS} "${LDFLAGS}")
+
+        # set(CFLAGS)
+        # string(APPEND CFLAGS " -march=nocona")
+        # string(APPEND CFLAGS " -msahf")
+        # string(APPEND CFLAGS " -mtune=generic")
+        # string(APPEND CFLAGS " -pipe")
+        # string(APPEND CFLAGS " -Wp,-D_FORTIFY_SOURCE=2")
+        # string(APPEND CFLAGS " -fstack-protector-strong")
+        # set(CFLAGS "${CFLAGS}")
+        # set(ENV{CFLAGS} "${CFLAGS}")
+
+        # set(CXXFLAGS)
+        # string(APPEND CXXFLAGS " -march=nocona")
+        # string(APPEND CXXFLAGS " -msahf")
+        # string(APPEND CXXFLAGS " -mtune=generic")
+        # string(APPEND CXXFLAGS " -pipe")
+        # set(CXXFLAGS "${CXXFLAGS}")
+        # set(ENV{CXXFLAGS} "${CXXFLAGS}")
 
         # Initial configuration flags.
         foreach(lang C CXX ASM Fortran OBJC OBJCXX)
@@ -140,53 +174,69 @@ if(NOT _MSYS_MINGW64_TOOLCHAIN)
             endif()
         endforeach()
 
-        string(APPEND CMAKE_C_FLAGS_INIT                    " ${MSYS_C_FLAGS} ")
-        string(APPEND CMAKE_C_FLAGS_DEBUG_INIT              " ${MSYS_C_FLAGS_DEBUG} ")
-        string(APPEND CMAKE_C_FLAGS_RELEASE_INIT            " ${MSYS_C_FLAGS_RELEASE} ")
-        string(APPEND CMAKE_C_FLAGS_MINSIZEREL_INIT         " ${MSYS_C_FLAGS_MINSIZEREL} ")
-        string(APPEND CMAKE_C_FLAGS_RELWITHDEBINFO_INIT     " ${MSYS_C_FLAGS_RELWITHDEBINFO} ")
+        string(APPEND CMAKE_C_FLAGS_INIT                        " ${MSYS_C_FLAGS} ")
+        string(APPEND CMAKE_C_FLAGS_DEBUG_INIT                  " ${MSYS_C_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_C_FLAGS_RELEASE_INIT                " ${MSYS_C_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_C_FLAGS_MINSIZEREL_INIT             " ${MSYS_C_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_C_FLAGS_RELWITHDEBINFO_INIT         " ${MSYS_C_FLAGS_RELWITHDEBINFO} ")
 
-        string(APPEND CMAKE_CXX_FLAGS_INIT                  " ${MSYS_CXX_FLAGS} ")
-        string(APPEND CMAKE_CXX_FLAGS_DEBUG_INIT            " ${MSYS_CXX_FLAGS_DEBUG} ")
-        string(APPEND CMAKE_CXX_FLAGS_RELEASE_INIT          " ${MSYS_CXX_FLAGS_RELEASE} ")
-        string(APPEND CMAKE_CXX_FLAGS_MINSIZEREL_INIT       " ${MSYS_CXX_FLAGS_MINSIZEREL} ")
-        string(APPEND CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT   " ${MSYS_CXX_FLAGS_RELWITHDEBINFO} ")
+        string(APPEND CMAKE_CXX_FLAGS_INIT                      " ${MSYS_CXX_FLAGS} ")
+        string(APPEND CMAKE_CXX_FLAGS_DEBUG_INIT                " ${MSYS_CXX_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_CXX_FLAGS_RELEASE_INIT              " ${MSYS_CXX_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_CXX_FLAGS_MINSIZEREL_INIT           " ${MSYS_CXX_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT       " ${MSYS_CXX_FLAGS_RELWITHDEBINFO} ")
 
-        string(APPEND CMAKE_OBJC_FLAGS_INIT                    " ${MSYS_OBJC_FLAGS} ")
-        string(APPEND CMAKE_OBJC_FLAGS_DEBUG_INIT              " ${MSYS_OBJC_FLAGS_DEBUG} ")
-        string(APPEND CMAKE_OBJC_FLAGS_RELEASE_INIT            " ${MSYS_OBJC_FLAGS_RELEASE} ")
-        string(APPEND CMAKE_OBJC_FLAGS_MINSIZEREL_INIT         " ${MSYS_OBJC_FLAGS_MINSIZEREL} ")
-        string(APPEND CMAKE_OBJC_FLAGS_RELWITHDEBINFO_INIT     " ${MSYS_OBJC_FLAGS_RELWITHDEBINFO} ")
+        string(APPEND CMAKE_ASM_FLAGS_INIT                      " ${MSYS_ASM_FLAGS} ")
+        string(APPEND CMAKE_ASM_FLAGS_DEBUG_INIT                " ${MSYS_ASM_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_ASM_FLAGS_RELEASE_INIT              " ${MSYS_ASM_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_ASM_FLAGS_MINSIZEREL_INIT           " ${MSYS_ASM_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_ASM_FLAGS_RELWITHDEBINFO_INIT       " ${MSYS_ASM_FLAGS_RELWITHDEBINFO} ")
 
-        string(APPEND CMAKE_OBJCXX_FLAGS_INIT                  " ${MSYS_OBJCXX_FLAGS} ")
-        string(APPEND CMAKE_OBJCXX_FLAGS_DEBUG_INIT            " ${MSYS_OBJCXX_FLAGS_DEBUG} ")
-        string(APPEND CMAKE_OBJCXX_FLAGS_RELEASE_INIT          " ${MSYS_OBJCXX_FLAGS_RELEASE} ")
-        string(APPEND CMAKE_OBJCXX_FLAGS_MINSIZEREL_INIT       " ${MSYS_OBJCXX_FLAGS_MINSIZEREL} ")
-        string(APPEND CMAKE_OBJCXX_FLAGS_RELWITHDEBINFO_INIT   " ${MSYS_OBJCXX_FLAGS_RELWITHDEBINFO} ")
+        string(APPEND CMAKE_Fortran_FLAGS_INIT                  " ${MSYS_Fortran_FLAGS} ")
+        string(APPEND CMAKE_Fortran_FLAGS_DEBUG_INIT            " ${MSYS_Fortran_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_Fortran_FLAGS_RELEASE_INIT          " ${MSYS_Fortran_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_Fortran_FLAGS_MINSIZEREL_INIT       " ${MSYS_Fortran_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_Fortran_FLAGS_RELWITHDEBINFO_INIT   " ${MSYS_Fortran_FLAGS_RELWITHDEBINFO} ")
 
-        string(APPEND CMAKE_RC_FLAGS_INIT                   " ${MSYS_RC_FLAGS} ")
-        string(APPEND CMAKE_RC_FLAGS_DEBUG_INIT             " ${MSYS_RC_FLAGS_DEBUG} ")
-        string(APPEND CMAKE_RC_FLAGS_RELEASE_INIT           " ${MSYS_RC_FLAGS_RELEASE} ")
-        string(APPEND CMAKE_RC_FLAGS_MINSIZEREL_INIT        " ${MSYS_RC_FLAGS_MINSIZEREL} ")
-        string(APPEND CMAKE_RC_FLAGS_RELWITHDEBINFO_INIT    " ${MSYS_RC_FLAGS_RELWITHDEBINFO} ")
+        string(APPEND CMAKE_OBJC_FLAGS_INIT                     " ${MSYS_OBJC_FLAGS} ")
+        string(APPEND CMAKE_OBJC_FLAGS_DEBUG_INIT               " ${MSYS_OBJC_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_OBJC_FLAGS_RELEASE_INIT             " ${MSYS_OBJC_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_OBJC_FLAGS_MINSIZEREL_INIT          " ${MSYS_OBJC_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_OBJC_FLAGS_RELWITHDEBINFO_INIT      " ${MSYS_OBJC_FLAGS_RELWITHDEBINFO} ")
 
-        string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT        " ${MSYS_LINKER_FLAGS} ")
-        string(APPEND CMAKE_STATIC_LINKER_FLAGS_INIT        " ${MSYS_LINKER_FLAGS} ")
-        string(APPEND CMAKE_MODULE_LINKER_FLAGS_INIT        " ${MSYS_LINKER_FLAGS} ")
-        string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT           " ${MSYS_LINKER_FLAGS} ")
+        string(APPEND CMAKE_OBJCXX_FLAGS_INIT                   " ${MSYS_OBJCXX_FLAGS} ")
+        string(APPEND CMAKE_OBJCXX_FLAGS_DEBUG_INIT             " ${MSYS_OBJCXX_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_OBJCXX_FLAGS_RELEASE_INIT           " ${MSYS_OBJCXX_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_OBJCXX_FLAGS_MINSIZEREL_INIT        " ${MSYS_OBJCXX_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_OBJCXX_FLAGS_RELWITHDEBINFO_INIT    " ${MSYS_OBJCXX_FLAGS_RELWITHDEBINFO} ")
 
-        if(MSYS_CRT_LINKAGE STREQUAL "static")
-            string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT    "-static ")
-            string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT       "-static ")
+        string(APPEND CMAKE_RC_FLAGS_INIT                       " ${MSYS_RC_FLAGS} ")
+        string(APPEND CMAKE_RC_FLAGS_DEBUG_INIT                 " ${MSYS_RC_FLAGS_DEBUG} ")
+        string(APPEND CMAKE_RC_FLAGS_RELEASE_INIT               " ${MSYS_RC_FLAGS_RELEASE} ")
+        string(APPEND CMAKE_RC_FLAGS_MINSIZEREL_INIT            " ${MSYS_RC_FLAGS_MINSIZEREL} ")
+        string(APPEND CMAKE_RC_FLAGS_RELWITHDEBINFO_INIT        " ${MSYS_RC_FLAGS_RELWITHDEBINFO} ")
+
+        string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT            " ${MSYS_LINKER_FLAGS} ")
+        string(APPEND CMAKE_STATIC_LINKER_FLAGS_INIT            " ${MSYS_LINKER_FLAGS} ")
+        string(APPEND CMAKE_MODULE_LINKER_FLAGS_INIT            " ${MSYS_LINKER_FLAGS} ")
+        string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT               " ${MSYS_LINKER_FLAGS} ")
+
+        if(OPTION_STRIP_BINARIES)
+            string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT               " --strip-all")
         endif()
 
-        foreach(type SHARED STATIC MODULE EXE)
-            # string(APPEND CMAKE_${type}_LINKER_FLAGS_DEBUG_INIT             " -g ")
-            string(APPEND CMAKE_${type}_LINKER_FLAGS_RELEASE_INIT           " -O3 ")
-            string(APPEND CMAKE_${type}_LINKER_FLAGS_MINSIZEREL_INIT        " -Os ")
-            string(APPEND CMAKE_${type}_LINKER_FLAGS_RELWITHDEBINFO_INIT    " -O2 ")
-            # string(APPEND CMAKE_${type}_LINKER_FLAGS_RELWITHDEBINFO_INIT    " -g ")
-        endforeach()
+        if(OPTION_STRIP_SHARED)
+            string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT            " --strip-unneeded")
+        endif()
+
+        if(OPTION_STRIP_STATIC)
+            string(APPEND CMAKE_STATIC_LINKER_FLAGS_INIT            " --strip-debug")
+        endif()
+
+        if(MSYS_CRT_LINKAGE STREQUAL "static")
+            string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT        " -static")
+            string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT           " -static")
+        endif()
 
         string(APPEND CMAKE_SHARED_LINKER_FLAGS_DEBUG_INIT              " ${MSYS_LINKER_FLAGS_DEBUG} ")
         string(APPEND CMAKE_SHARED_LINKER_FLAGS_RELEASE_INIT            " ${MSYS_LINKER_FLAGS_RELEASE} ")
@@ -208,7 +258,10 @@ if(NOT _MSYS_MINGW64_TOOLCHAIN)
         string(APPEND CMAKE_EXE_LINKER_FLAGS_MINSIZEREL_INIT            " ${MSYS_LINKER_FLAGS_MINSIZEREL} ")
         string(APPEND CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO_INIT        " ${MSYS_LINKER_FLAGS_RELWITHDEBINFO} ")
 
-        unset(LDFLAGS)
+        # unset(LDFLAGS)
+        # unset(CFLAGS)
+        # unset(CXXFLAGS)
+        # unset(CPPFLAGS)
 
     endif()
 
